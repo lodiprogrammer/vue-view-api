2e3a12e93193c9e067fd3c078312556624bb652c
lodiprogrammer <114732032+lodiprogrammer@users.noreply.github.com>
2025-01-09 15:01:19 +0700

    first commit

---
+++ kuliner/src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <Navbar />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Navbar from '@/components/Navbar.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    Navbar
+  }
+}
+</script>
